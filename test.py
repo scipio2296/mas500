6d846830383cc0ee3adc767eeedd3b3c1945676e
scipio2296 <scipio2296@gmail.com>
2015-11-20 10:07:45 -0500

    test file

---
+++ test.py
@@ -0,0 +1,31 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import MC_HW1intermediate
+
+
+# In[2]:
+
+import unittest
+
+
+# In[4]:
+
+class TestAPICall (unittest.TestCase):
+	def testAPICall (self):
+		res =  MC_HW1intermediate
+		assert res!=None
+
+
+# In[6]:
+
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
+
+
+
